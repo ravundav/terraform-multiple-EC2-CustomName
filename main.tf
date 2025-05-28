0226ca6ea3351ce59a2a1bb1b910a6ad2ff13484
ravundav <mycloud447@gmail.com>
2025-05-28 21:28:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,27 @@
+provider "aws" {
+  region = "us-east-1"
+access_key = var.aws_access_key
+secret_key = var.aws_secret_key
 
+}
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+resource "aws_instance" "aws-training" {
+  count         = 28
+  ami           = "ami-084568db4383264d4"
+  instance_type = "t2.medium"
+  key_name      = "Trng-rk"
+
+  tags = {
+    #Name = "docker-${count.index + 1}"
+    Name = var.instance_names[count.index]
+  }
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo apt-get update -y
+    sudo curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sudo sh get-docker.sh
+  EOF
+}
