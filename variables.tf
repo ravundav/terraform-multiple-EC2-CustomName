6153a1fa20e074d6b58c056a8d21184e0cf30983
ravundav <mycloud447@gmail.com>
2025-05-28 21:29:13 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "instance_names" {
+  type    = list(string)
+  default = ["Docker-Jyotsna", "Docker-SaiCharan", "Docker-Tharun", "Docker-Mrudula", "Docker-Madhavi", "Docker-Nagarjuna", "Docker-LakshmiBhavani", "Docker-Mercy", "Docker-Balakrishna", "Docker-Kommireddy", "Docker-Prashanth", "Docker-Uday", "Docker-KarthikRaju", "Docker-Ramesh", "Docker-TulasiLakshmi", "Docker-ShravanKumar", "Docker-Prasad", "Docker-Puja", "Docker-KalluSrujan", "Docker-Vijaykanth", "Docker-Nandini", "Docker-Saikiran", "Docker-NagaSaiNikhil", "Docker-Vimal", "Docker-SohithaSaiSree", "Docker-NaveenChandra", "Docker-Manikantha", "Docker-Anaparthi"]
+}
